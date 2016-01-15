4256819545964c012ef4daedad6d5808242d182f
Kuspakov Artur <A.Kuspakov@holding-progress.com>
2016-01-15 15:59:05 +0300

    explicit migrations

--- AutomatedTellerMachine/Migrations/201601151254085_InitialCreate.cs
+++ AutomatedTellerMachine/Migrations/201601151254085_InitialCreate.cs
@@ -12,21 +12,34 @@ namespace AutomatedTellerMachine.Migrations
                 c => new
                     {
                         Id = c.Int(nullable: false, identity: true),
-                        AccountNumber = c.String(nullable: false),
+                        AccountNumber = c.String(nullable: false, maxLength: 10, unicode: false),
                         FirstName = c.String(nullable: false),
                         LastName = c.String(nullable: false),
                         Balance = c.Decimal(nullable: false, precision: 18, scale: 2),
-                        ApplicationUserId = c.String(maxLength: 128),
+                        ApplicationUserId = c.String(nullable: false, maxLength: 128),
                     })
                 .PrimaryKey(t => t.Id)
-                .ForeignKey("dbo.AspNetUsers", t => t.ApplicationUserId)
+                .ForeignKey("dbo.AspNetUsers", t => t.ApplicationUserId, cascadeDelete: true)
                 .Index(t => t.ApplicationUserId);
+            
+            CreateTable(
+                "dbo.Transactions",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Amount = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        CheckingAccountId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.CheckingAccounts", t => t.CheckingAccountId, cascadeDelete: true)
+                .Index(t => t.CheckingAccountId);
             
             CreateTable(
                 "dbo.AspNetUsers",
                 c => new
                     {
                         Id = c.String(nullable: false, maxLength: 128),
+                        Pin = c.String(),
                         Email = c.String(maxLength: 256),
                         EmailConfirmed = c.Boolean(nullable: false),
                         PasswordHash = c.String(),
@@ -99,18 +112,21 @@ namespace AutomatedTellerMachine.Migrations
             DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
             DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
             DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Transactions", "CheckingAccountId", "dbo.CheckingAccounts");
             DropIndex("dbo.AspNetRoles", "RoleNameIndex");
             DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
             DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
             DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
             DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
             DropIndex("dbo.AspNetUsers", "UserNameIndex");
+            DropIndex("dbo.Transactions", new[] { "CheckingAccountId" });
             DropIndex("dbo.CheckingAccounts", new[] { "ApplicationUserId" });
             DropTable("dbo.AspNetRoles");
             DropTable("dbo.AspNetUserRoles");
             DropTable("dbo.AspNetUserLogins");
             DropTable("dbo.AspNetUserClaims");
             DropTable("dbo.AspNetUsers");
+            DropTable("dbo.Transactions");
             DropTable("dbo.CheckingAccounts");
         }
     }
